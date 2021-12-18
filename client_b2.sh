28e15189e25eefe55ac01f73753d7842c4d1f675
Bipin Khatiwada <bipinkh91@gmail.com>
2021-12-18 21:06:19 +0200

    Write all the scripts and http files

---
+++ client_b2.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+echo ""
+echo 
+# Check cURL command if available (required), abort if does not exists
+type curl >/dev/null 2>&1 || { echo >&2 "Required curl but it's not installed. Aborting."; exit 1; }
+apt install jq
+
+echo
+
+# environment variables
+OVERLAY_ID="431165ef939e46d090a04882331c33ef"
+DEVICE_ID="e82bec829c1a4f4498674284ac6c7390"
+API_KEY="BC0B9f28FbD28CDC81c441A2db5Ea950"
+EXPORT_CONF_LOCATION="/etc/wireguard/wg0.conf"
+#EXPORT_CONF_LOCATION="client_b2_w0.conf"
+
+# header components
+HEADER_CONTENTTYPE="Content-Type: application/json"
+HEADER_APIKEY="x-api-key: "${API_KEY}
+
+# urls
+HOST="http://meshmash.vikaa.fi:49324"
+URI_GET_CONFIG=${HOST}"/overlays/"${OVERLAY_ID}"/devices/"${DEVICE_ID}"/wgconfig"
+URI_GET_TOKEN=${HOST}"/devices/"${DEVICE_ID}"/token"
+URI_PUT_IP=${HOST}"/devices/"${DEVICE_ID}
+
+
+
+
+# GET THE TOKEN
+echo "getting the token.."
+response=$(curl -s --request GET -H "${HEADER_CONTENTTYPE}" -H "${HEADER_APIKEY}" ${URI_GET_TOKEN} | jq -r '.token')
+echo $response
+TOKEN=$response
+
+echo "--------"
+
+# GET THE CONFIG
+
+BEARER_HEADER="Authorization: Bearer "${TOKEN}
+echo "getting the wconfig.."
+response1=$(curl -s --request GET -H "${HEADER_CONTENTTYPE}" -H "${HEADER_APIKEY}" -H "${BEARER_HEADER}" ${URI_GET_CONFIG} > ${EXPORT_CONF_LOCATION})
+echo "Fetched the wconfig for device" ${DEVICE_ID} "of overlay" ${OVERLAY_ID} "and updated " ${EXPORT_CONF_LOCATION}
